d1bf370fc4d8640eac452ce682d6a4d562a2fb8b
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-05-25 06:40:26 +0500

    update

--- src/ocr.py
+++ src/ocr.py
@@ -1,5 +1,5 @@
 import pytesseract
-from src.preprocess import preprocess_image, extract_text_from_pdf
+from src.preprocess import extract_text_from_pdf
 import logging, os
 
 os.makedirs(os.path.join("logs"), exist_ok=True)  # 📂 Creates a 'logs' directory if it doesn't exist
@@ -16,14 +16,14 @@ def extract_text(file_path: str) -> str:
     """Extract text from image or PDF."""
     logging.info(f"Starting text extraction for file: {file_path}")
     try:
-        if file_path.lower().endswith((".png", ".jpg", ".jpeg")):
-            img = preprocess_image(file_path)
-            if img is None:
-                logging.warning("No image data after preprocessing")
-                return ""
-            text = pytesseract.image_to_string(img)
-            logging.info("Text extracted from image")
-        elif file_path.lower().endswith(".pdf"):
+        # if file_path.lower().endswith((".png", ".jpg", ".jpeg")):
+        #     img = preprocess_image(file_path)
+        #     if img is None:
+        #         logging.warning("No image data after preprocessing")
+        #         return ""
+        #     text = pytesseract.image_to_string(img)
+        #     logging.info("Text extracted from image")
+        if file_path.lower().endswith(".pdf"):
             text = extract_text_from_pdf(file_path)
             logging.info("Text extracted from PDF")
         else:
